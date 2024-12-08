1b79783ab44740b97695467ffa8a405ded4bff35
aimanyusufw <aimanyusufdev@gmail.com>
2024-12-08 20:16:08 +0700

    style : banner carousel in home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import BaseLayouts from "../Layouts/BaseLayouts";
+import { Carousel } from "flowbite-react";
 
 const Home = () => {
   return (
     <BaseLayouts>
-      <h1 className="font-bold">Home Page</h1>
+      <section className="py-4">
+        <div className="container">
+          <div className="h-56 sm:h-64 lg:h-96 2xl:h-[28rem]">
+            <Carousel indicators={false} pauseOnHover>
+              <img
+                src="Banner_1.svg"
+                className="w-full"
+                alt="Smart watch banner"
+              />
+              <img
+                src="Banner_2.svg"
+                className="w-full"
+                alt="iphone new release banner"
+              />
+              <img
+                src="Banner_3.svg"
+                className="w-full"
+                alt="Playsatation 5 best seller banner"
+              />
+            </Carousel>
+          </div>
+        </div>
+      </section>
     </BaseLayouts>
   );
 };
