a79c22f543175d19e41183ac90738288a2d61643
aimanyusufw <aimanyusufdev@gmail.com>
2025-01-18 16:41:41 +0700

    ref : refacto Navabar add Seaarch input

--- src/Components/Ui/Section/Navbar.jsx
+++ src/Components/Ui/Section/Navbar.jsx
@@ -1,39 +1,56 @@
 import React from "react";
-import {
-  Button,
-  Navbar,
-  NavbarBrand,
-  NavbarCollapse,
-  NavbarLink,
-  NavbarToggle,
-} from "flowbite-react";
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import SearchInput from "../../Utils/SearchInput";
 import { FaCartShopping } from "react-icons/fa6";
 
 const NavbarComp = () => {
   return (
-    <Navbar rounded className="px-0 md:px-4">
-      <NavbarBrand href="/">
-        <img src="/Logo.svg" className="h-6 md:h-8" alt="" />
-      </NavbarBrand>
-      <div className="flex items-center md:order-2 gap-2">
-        <a href="/cart" className="me-2 relative py-3">
+    <Navbar rounded className="md:px-6">
+      <Navbar.Brand href="/" className="hidden md:block">
+        <img
+          src="/Logo.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Flowbite React Logo"
+        />
+      </Navbar.Brand>
+      <div className="flex w-full md:w-3/4 lg:w-1/2 md:order-2">
+        <SearchInput />
+        <a
+          href="/cart"
+          type="button"
+          className="relative inline-flex items-center ext-sm font-medium text-center mx-4"
+        >
           <FaCartShopping className="w-5 h-5" />
-          <span className="absolute top-1 left-2 px-1.5  bg-red-500 rounded-full text-xs text-white">
-            1
-          </span>
+          <span class="sr-only">Notifications</span>
+          <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
+            20
+          </div>
         </a>
-        <Button color="gray">
-          <a href="/signup">Join Now</a>
-        </Button>
-        <NavbarToggle />
+
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar
+              alt="User settings"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded
+            />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">
+              name@flowbite.com
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Profile</Dropdown.Item>
+          <Dropdown.Item>Order</Dropdown.Item>
+          <Dropdown.Item href="/cart">Cart</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
       </div>
-      <NavbarCollapse>
-        <NavbarLink href="/" active>
-          Home
-        </NavbarLink>
-        <NavbarLink href="/products">Products</NavbarLink>
-        <NavbarLink href="/contatc-us">Contant Us</NavbarLink>
-      </NavbarCollapse>
     </Navbar>
   );
 };
