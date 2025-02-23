2d56a97e3fb97de5b2792d1a03f7240a5128709a
aimanyusufw <aimanyusufdev@gmail.com>
2025-02-23 16:17:38 +0700

    ref<style> : make order can rander mant order items

--- src/Pages/Auth/Profile.jsx
+++ src/Pages/Auth/Profile.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import BaseLayouts from "../../Layouts/BaseLayouts";
 import { Avatar } from "flowbite-react";
 import {
+  FaArrowRight,
   FaCartShopping,
-  FaGear,
   FaMapLocation,
   FaMoneyBill,
   FaUserGear,
@@ -38,31 +38,69 @@ const Profile = () => {
             icon={<FaCartShopping className="w-6 h-6 text-neutral-700" />}
             defaultOpen={true}
           >
-            <div className="w-1/2">
-              <div className="p-5 border rounded-md flex">
-                <img
-                  src="https://dlcdnwebimgs.asus.com/gain/d5e3167c-efa8-46ce-bd78-65a4e99af3f6/w800"
-                  alt=""
-                  className="w-1/4 bg-slate-200 rounded-sm"
-                />
-                <div className="ms-4 space-y-3 w-1/2">
-                  <h1 className="font-medium text-sm md:text-lg ">
-                    ASUS TUF GAMING A16
-                  </h1>
-                  <p className="font-medium text-xs md:text-sm">
-                    Quantitiy : 1
-                  </p>
-                  <h2 className="font-medium text-xs md:text-sm">
-                    Rp. 16.000.000,00
-                  </h2>
-                  <h1 className="font-medium text-sm">
-                    Order Status : <span className="px-2 py-1">Shipped</span>
-                  </h1>
-                </div>
-                <div className="ms-auto">
+            <div className="w-full">
+              <div className="p-5 border rounded-md">
+                <div className="flex gap-4 mb-6">
+                  <h1>23 Februari 2025 </h1>
                   <span className="rounded-full font-medium text-xs px-3 py-1 bg-yellow-300 text-yellow-700">
                     Shipped
                   </span>
+                  <h1>INV/2123123/123123</h1>
+                </div>
+                <div className="space-y-6">
+                  <div className="flex justify-between items-center">
+                    <div className="flex">
+                      <img
+                        src="https://dlcdnwebimgs.asus.com/gain/d5e3167c-efa8-46ce-bd78-65a4e99af3f6/w800"
+                        alt=""
+                        className="w-32 bg-slate-200 rounded-sm"
+                      />
+                      <div className="ms-4 space-y-3">
+                        <h1 className="font-medium text-sm md:text-lg ">
+                          ASUS TUF GAMING A16
+                        </h1>
+                        <p className="font-medium text-xs md:text-sm">
+                          Quantitiy : 1
+                        </p>
+                        <h2 className="font-medium text-xs md:text-sm">
+                          Rp. 16.000.000,00
+                        </h2>
+                      </div>
+                    </div>
+                    <h2 className="font-semibold text-base md:text-lg">
+                      Rp. 16.000.000,00
+                    </h2>
+                  </div>
+                  <div className="flex justify-between items-center">
+                    <div className="flex">
+                      <img
+                        src="https://dlcdnwebimgs.asus.com/gain/d5e3167c-efa8-46ce-bd78-65a4e99af3f6/w800"
+                        alt=""
+                        className="w-32 bg-slate-200 rounded-sm"
+                      />
+                      <div className="ms-4 space-y-3">
+                        <h1 className="font-medium text-sm md:text-lg ">
+                          ASUS TUF GAMING A16
+                        </h1>
+                        <p className="font-medium text-xs md:text-sm">
+                          Quantitiy : 1
+                        </p>
+                        <h2 className="font-medium text-xs md:text-sm">
+                          Rp. 16.000.000,00
+                        </h2>
+                      </div>
+                    </div>
+                    <h2 className="font-semibold text-base md:text-lg">
+                      Rp. 16.000.000,00
+                    </h2>
+                  </div>
+                  <button
+                    href="/"
+                    className="py-3 px-6 border-2 font-medium border-black rounded-lg"
+                  >
+                    Transaction Detail
+                    <FaArrowRight className="inline ms-4" />
+                  </button>
                 </div>
               </div>
             </div>
