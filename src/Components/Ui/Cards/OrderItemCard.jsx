271ba06eef786d009fc0f01217a328bd0ee52c27
aimanyusufw <aimanyusufdev@gmail.com>
2025-03-05 23:59:30 +0700

    add : order item card

---
+++ src/Components/Ui/Cards/OrderItemCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const OrderItemCard = ({ data }) => {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex">
+        <img
+          src="https://dlcdnwebimgs.asus.com/gain/d5e3167c-efa8-46ce-bd78-65a4e99af3f6/w800"
+          alt={data.name}
+          className="w-32 bg-slate-200 rounded-sm"
+        />
+        <div className="ms-4 space-y-3">
+          <h1 className="font-medium text-sm md:text-lg">{data.name}</h1>
+          <p className="font-medium text-xs md:text-sm">
+            Quantity: {data.quantity}
+          </p>
+          <h2 className="font-medium text-xs md:text-sm">
+            {data.price.toLocaleString("id-ID", {
+              style: "currency",
+              currency: "IDR",
+            })}
+          </h2>
+        </div>
+      </div>
+      <h2 className="font-semibold text-base md:text-lg">
+        {(data.price * data.quantity).toLocaleString("id-ID", {
+          style: "currency",
+          currency: "IDR",
+        })}
+      </h2>
+    </div>
+  );
+};
+
+export default OrderItemCard;
