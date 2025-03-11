17bb896dec963217823e0a33dbff3968c0c0fa9a
aimanyusufw <aimanyusufdev@gmail.com>
2025-03-11 14:32:54 +0700

    ref<style> : remove dropdown from search

--- src/Components/Utils/SearchInput.jsx
+++ src/Components/Utils/SearchInput.jsx
@@ -1,44 +1,25 @@
-import { Dropdown } from "flowbite-react";
+import { TextInput } from "flowbite-react";
 import React from "react";
-import { FaChevronDown } from "react-icons/fa6";
+import { FiSearch } from "react-icons/fi";
 
 const SearchInput = () => {
   return (
-    <form className="max-w-lg w-full mx-auto">
+    <form className="w-full mx-auto">
       <div className="flex">
         <label
           for="search-dropdown"
           className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
         >
-          Your Email
+          Search
         </label>
-
-        <Dropdown
-          label=""
-          dismissOnClick={false}
-          renderTrigger={() => (
-            <button
-              className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
-              type="button"
-            >
-              All Categories <FaChevronDown className="ms-2 w-2 h-2" />
-            </button>
-          )}
-        >
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Item>Sign out</Dropdown.Item>
-        </Dropdown>
-        <div className="relative w-full">
-          <input
-            type="search"
-            id="search-dropdown"
-            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-            placeholder="Search Products..."
-            required
-          />
-        </div>
+        <TextInput
+          className="w-full"
+          id="email4"
+          type="search"
+          icon={FiSearch}
+          placeholder="Find what you want.."
+          required
+        />
       </div>
     </form>
   );
